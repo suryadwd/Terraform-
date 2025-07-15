6afae74bf366aa1545c4acb1ea5ccd813e39be44
suryadwd <surajdwivedi644@gmail.com>
2025-07-15 08:38:08 +0530

    day2 of goal

--- ngnix.sh
+++ ngnix.sh
@@ -1,8 +1,6 @@
-  
-    sudo apt update -y
-    sudo apt install nginx -y
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-    echo "<h1> Learning terraform  </h1>"  > /var/www/html/index.html
-    # echo "<h1> Learning terraform  </h1>"  | sudo tee /var/www/html/index.html   # tee -> is use to append the in the given lovationclear
-    #updated 
+#!/bin/bash
+apt update -y
+apt install nginx -y
+systemctl enable nginx
+systemctl start nginx
+echo "<h1> Learning terraform </h1>" > /var/www/html/index.html
