316ec5c7ebc56ba97dda9f34458bdeffcc4be7f2
suryadwd <surajdwivedi644@gmail.com>
2025-07-15 19:00:55 +0530

    day2 of goals

--- output.tf
+++ output.tf
@@ -32,3 +32,13 @@ output "ec2_private_ip" {
     for key in aws_instance.my_instance : key.private_ip
   ]
 }
+
+
+output "ec2_volume_sizes" {
+  value = {
+    for key, instance in aws_instance.my_instance :
+    key => instance.root_block_device[0].volume_size
+  }
+}
+
+
