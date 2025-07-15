24ff0b9c1c53a8dac02f6387ba1a5ab34d6672c8
suryadwd <surajdwivedi644@gmail.com>
2025-07-15 08:15:54 +0530

    day2 of goal

--- ngnix.sh
+++ ngnix.sh
@@ -3,4 +3,5 @@
     sudo pacman -S nginx --noconfirm
     sudo systemctl enable --now nginx
     echo "<h1> Learning terraform  </h1>"  > /var/www/html/index.html
-    # echo "<h1> Learning terraform  </h1>"  | sudo tee /var/www/html/index.html   # tee -> is use to append the in the given lovation
+    # echo "<h1> Learning terraform  </h1>"  | sudo tee /var/www/html/index.html   # tee -> is use to append the in the given lovationclear
+    #updated 
